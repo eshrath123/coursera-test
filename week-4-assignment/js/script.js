d740ddeb58aee57fc12dda0b1233c4808a9cf486
keshrathk <eshrath123@gmail.com>
2017-02-27 14:37:43 +0530

    Module three assignment.

---
+++ week-4-assignment/js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+ if(name.charAt(0) == "J" || name.charAt(0) == "j"){
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += " & Lovin' it!";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+    else{  
+document.write("goodbye"+name);  
+} 
+}
